e2983b619a35fbbc2f0cfeb3858120ed6a24326e
Piotr <piotrwanio@gmail.com>
2019-02-14 16:26:04 +0100

    first commit

---
+++ SportStore.Domain/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportStore.Domain.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Piłka nożna", Price = 25},
+                new Product {Name = "Deska snowboardowa", Price = 2222, },
+                new Product {Name = "Buty do biegania", Price = 22}
+            }.AsQueryable());
+
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+    }
+}
---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16d7875a-4db1-4d97-9066-732f5d291602")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
