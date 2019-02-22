0ae45f94be41ea565147f3f40c03f4b3e86bade2
Piotr <piotrwanio@gmail.com>
2019-02-22 19:30:51 +0100

    navigation controller and views added, cart's first version

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Web.Mvc;
 using SportsStore.WebUI.Models;
 using SportsStore.WebUI.HtmlHelpers;
+using SportStore.Domain.Controllers;
 
 namespace SportsStore.UnitTests
 {
@@ -34,7 +35,7 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             // doing
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
 
             //asserts
             Product[] prodArray = result.Products.ToArray();
@@ -62,6 +63,120 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(result.ToString(), @"<a href=""Strona1"">1</a>"
                 + @"<a class=""selected"" href=""Strona2"">2</a>"
                 + @"<a href=""Strona3"">3</a>");
+        }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            // preparation
+            // imitate repository creating
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            }.AsQueryable());
+
+            // getting ready
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // doing
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model)
+                .Products.ToArray();
+
+            // asserts
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+
+        }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // preparation - creating repo
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"}
+            }.AsQueryable());
+
+            // preparation - creating controller
+            NavController target = new NavController(mock.Object);
+
+            // doing - load categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // asserts
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            //preparation
+            // creating repo imitation
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"}
+            }.AsQueryable());
+
+            // preparation - creating controller
+            NavController target = new NavController(mock.Object);
+
+            //preparation - fefinition of choosed category
+            string categoryToSelect = "Apples";
+
+            // doing - load categories
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            // asserts
+            Assert.AreEqual(categoryToSelect, result);
+        }
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //preparation
+            // creating repo imitation
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            }.AsQueryable());
+
+            // preparation - creating controller
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            // doing
+            int res1 = ((ProductsListViewModel)target.List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductsListViewModel)target.List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductsListViewModel)target.List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductsListViewModel)target.List(null).Model).PagingInfo.TotalItems;
+
+            // asserts
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
+
         }
     }
 }
--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,16 +14,37 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: null,
-                url: "Strona{page}",
-                defaults: new {controller = "Product", action = "List"}
+                null,
+                "",
+                new {controller = "Product", action = "List",
+                    category = (string)null, page = 1
+                }
             );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+                null,
+                "Strona{page}",
+                new {controller = "Product", action = "List",
+                    category = (string)null
+                },
+                new {page = @"\d+"}
             );
+
+            routes.MapRoute(
+                null,
+                url: "{category}",
+                defaults: new { controller = "Product", action = "List", page = 1}
+            );
+
+            routes.MapRoute(
+                null,
+                url: "{category}/Strona{page}",
+                defaults: new { controller = "Product", action = "List"},
+                constraints: new { page = @"\d+"}
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
+
         }
     }
 }
